13fc02bd2752a07f41b7ff9685ff4fd6e7055b7e
Lavken <lavkenpilar@gmail.com>
2024-11-17 23:40:44 -0300

    create-className-css

--- trabajando_con_jsx/src/components/Producto.jsx
+++ trabajando_con_jsx/src/components/Producto.jsx
@@ -14,4 +14,17 @@ const Producto = () => {
   const productName="Producto 1"
   return( <h1>{productName}</h1>)
 }*/
+const Producto=()=>{
+  const productName = "Producto 1"
+  return (
+    <>
+    <h1 className='producto'>{productName}</h1>
+    <p>Operaciones matemáticas Suma:{2+2}</p>
+    <p>Operaciones matemáticas Resta:{2-2}</p>
+    <p>Operaciones matemáticas Multiplicacion:{2*2}</p>
+    <p>Operaciones matemáticas División:{2 / 2}</p>
+    <p>Operaciones matemáticas Módulo:{2%2}</p>
+    </>
+  )
+}
 export default Producto
