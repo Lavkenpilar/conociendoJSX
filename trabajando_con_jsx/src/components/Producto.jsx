1332f052d993ad2af39b7fb2d1caf3358c691acf
Lavken <lavkenpilar@gmail.com>
2024-11-18 21:32:57 -0300

    create-style-myStyle

--- trabajando_con_jsx/src/components/Producto.jsx
+++ trabajando_con_jsx/src/components/Producto.jsx
@@ -16,15 +16,17 @@ const Producto = () => {
 }*/
 const Producto=()=>{
   const productName = "Producto 1"
+  const myStyle = {fontSize:'12px', color:'brown'}
   return (
     <>
     <h1 className='producto'>{productName}</h1>
-    <p>Operaciones matemáticas Suma:{2+2}</p>
-    <p>Operaciones matemáticas Resta:{2-2}</p>
+    <p style={{fontSize:'12px', color:'blue'}}>Operaciones matemáticas Suma:{2+2}</p>
+    <p style={myStyle}>Operaciones matemáticas Resta:{2-2}</p>
     <p>Operaciones matemáticas Multiplicacion:{2*2}</p>
     <p>Operaciones matemáticas División:{2 / 2}</p>
     <p>Operaciones matemáticas Módulo:{2%2}</p>
     </>
   )
 }
+
 export default Producto
